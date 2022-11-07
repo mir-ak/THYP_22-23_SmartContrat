6d3c9c2c64b7ba3746b49e8579bf674a6162fa34
mir-ak <mirakcr7@gmail.com>
2022-11-07 11:50:38 +0100

    update format

--- truffle-config.js
+++ truffle-config.js
@@ -3,22 +3,22 @@ module.exports = {
     development: {
       host: "127.0.0.1",
       port: 8545,
-      network_id: "*"
+      network_id: "*",
     },
     dashboard: {},
     loc_dev_dev: {
       network_id: "*",
       port: 8001,
-      host: "127.0.0.1"
-    }
+      host: "127.0.0.1",
+    },
   },
   compilers: {
     solc: {
-      version: "0.8.13"
-    }
+      version: "0.8.13",
+    },
   },
   db: {
     enabled: false,
-    host: "127.0.0.1"
-  }
+    host: "127.0.0.1",
+  },
 };
